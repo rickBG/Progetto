a04907bfea48a6adc74c524268b4c9f507709420
Teshta <Damiano@Damiano-HP>
2018-05-16 08:24:46 +0200

    aggiunta classe responsabile

--- progettoSiw/src/model/Responsabile.java
+++ progettoSiw/src/model/Responsabile.java
@@ -1,5 +1,6 @@
 package model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -11,4 +12,40 @@ public class Responsabile {
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Long id;
+	
+	@Column(nullable = false)
+	private String nome;
+	
+	@Column(nullable = false)
+	private String ruolo;
+
+	public Responsabile(Long id, String nome, String ruolo) {
+		this.id = id;
+		this.nome = nome;
+		this.ruolo = ruolo;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getRuolo() {
+		return ruolo;
+	}
+
+	public void setRuolo(String ruolo) {
+		this.ruolo = ruolo;
+	}	
 }
