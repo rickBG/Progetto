a9170cfc9bf0755111db912a8e27e8bcdf914e66
rick <riccardo.bianchini78@gmail.com>
2018-05-30 11:10:32 +0200

    prova

--- progettoSiw/src/model/Allievo.java
+++ progettoSiw/src/model/Allievo.java
@@ -3,15 +3,13 @@ package model;
 import java.util.Date;
 import java.util.List;
 
-import javax.persistence.CascadeType;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
